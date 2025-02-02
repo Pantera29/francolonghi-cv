f90aef9daa9f3ebff903435f842785a052a3c812
Fermin Rodriguez Penelas <frodriguezpenelas@gmail.com>
2025-02-02 18:56:30 -0300

    Add metadata for Cafecito donations page

--- src/app/cafecito/page.tsx
+++ src/app/cafecito/page.tsx
@@ -1,4 +1,9 @@
 import { CafecitoDonations } from "./donations";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Invitame un Cafecito - @ferminrp",
+};
 
 export default function CafecitoDonationsPage() {
   return <CafecitoDonations />;
